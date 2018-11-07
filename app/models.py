43b1dcc3a201aee50ffc242e26479ab1948e3c7c
blues <470960070@qq.com>
2018-11-07 14:34:16 +0800

    更新用户系统 -db似乎有点问题

--- app/models.py
+++ app/models.py
@@ -1,8 +1,10 @@
 from datetime import datetime
+from werkzeug.security import generate_password_hash, check_password_hash
 from app import db
+from flask_login import UserMixin
+from app import login
 
-
-class User(db.Model):
+class User(UserMixin,db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), index=True, unique=True)
     email = db.Column(db.String(120), index=True, unique=True)
@@ -12,6 +14,11 @@ class User(db.Model):
     def __repr__(self):
         return '<User %s>' % self.username
 
+    def set_password(self,password):
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self,password):
+        return check_password_hash(self.password_hash,password)
 
 class Post(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -21,3 +28,7 @@ class Post(db.Model):
 
     def __repr__(self):
         return '<Post %s>' % self.body
+
+@login.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
