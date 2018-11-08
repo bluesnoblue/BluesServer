5dadc728f2938ff14cc320f10746267fcc8ebcbe
blues <470960070@qq.com>
2018-11-09 00:14:58 +0800

    FIXBUG 发现页分页连接有误

--- app/routes.py
+++ app/routes.py
@@ -33,6 +33,16 @@ def index():
                            next_url=next_url, prev_url=prev_url)
 
 
+@app.route('/explore', methods=['GET', 'POST'])
+@login_required
+def explore():
+    page = request.args.get('page', 1, type=int)
+    posts = Post.query.order_by(Post.timestamp.desc()).paginate(page, app.config['POST_PER_PAGE'], False)
+    next_url = url_for('explore', page=posts.next_num) if posts.has_next else None
+    prev_url = url_for('explore', page=posts.prev_num) if posts.has_prev else None
+    return render_template('index.html', title='Explore', posts=posts.items, next_url=next_url, prev_url=prev_url)
+
+
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     if current_user.is_authenticated:
@@ -132,11 +142,4 @@ def unfollow(username):
     return redirect(url_for('user', username=username))
 
 
-@app.route('/explore', methods=['GET', 'POST'])
-@login_required
-def explore():
-    page = request.args.get('page', 1, type=int)
-    posts = Post.query.order_by(Post.timestamp.desc()).paginate(page, app.config['POST_PER_PAGE'], False)
-    next_url = url_for('explore', page=posts.next_num) if posts.has_next else None
-    prev_url = url_for('explore', page=posts.prev_num) if posts.has_prev else None
-    return render_template('index.html', title='Explore', posts=posts.items, next_ur=next_url, prev_url=prev_url)
+
