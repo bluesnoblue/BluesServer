5b67e102a47e05446dfe80569217bc3c38fd541f
blues <470960070@qq.com>
2018-11-09 16:11:45 +0800

    优化应用结构 app实例工厂函数

--- app/main/routes.py
+++ app/main/routes.py
@@ -1,6 +1,6 @@
 from flask import redirect,url_for,flash,request,render_template
 from flask_login import login_required,current_user
-from app import app, db
+from app import db
 from app.main import bp
 from app.main.forms import PostForm, EditProfileForm
 from app.models import User, Post
